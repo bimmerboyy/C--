380b5c528104e90e8b1f8b4d0ae60d1be56a6337
anonymousvegan <anonymousvegan>
2022-02-23 10:42:30 +0100

    finished 6th

---
+++ sesti.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main(){
+    int a, b;
+    char c;
+    cout << "Unesite a i b";
+    cin  >>a>>b;
+    cout << "Unesite operaciju";
+    cin >>c;
+    switch(c){
+        case '+':
+        cout<< a+b;
+        break;
+        case '-':
+        cout<< a-b;
+        break;
+        case '*':
+        cout<< a*b;
+        break;
+        case '/':
+        cout<< a/b;
+        break;
+        case '%':
+        cout<< a%b;
+        break;
+    }
+    return 0; 
+}
