a748987f8b243f5e1a2fa8663358b0573e93d312
bimmer <tarikkucevic@gmail.com>
2022-03-01 10:21:13 +0100

    training c++

---
+++ vezbanje06.cpp
@@ -0,0 +1,26 @@
+/*8.1.. Napisati program za određjivanje najmanjeg broja od n učitanih.*/
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b,c,min;
+    cout<<"Unesite a:";
+    cin>>a;
+    cout<<"Unesite b:";
+    cin>>b;
+    cout<<"Unesite c:";
+    cin>>c;
+    if(a<min){
+        min=a;
+    }
+    else if(b<min){
+        min=b;
+
+    }
+    else if(c<min){
+        min=c;
+    }
+    cout<<"Min je:"<<min;
+    return 0;
+
+}
+
