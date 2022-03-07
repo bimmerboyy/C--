bd6474dd6a8990406bcde59b1e0fca7a290ca06a
bimmer <tarikkucevic@gmail.com>
2022-03-07 18:20:35 +0100

    exercise

--- vezbanje/nikola.cpp
+++ vezbanje/nikola.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void main(){
+int main(){
     cout << "Reeeeeši";
+    return 0;
 }
---
+++ vezbanje/vezbanje08.cpp
@@ -0,0 +1,15 @@
+//p08111039//
+#include <iostream>
+
+using namespace std;
+int main()
+{
+int i,a;
+cout<<"Djeljivi sa brojem?"<<endl;
+cin>>a;
+    for(i=1;i<=17;i++){
+         if(i % a ==0)
+        cout<<i<<endl;
+    }
+    return 0;
+}
