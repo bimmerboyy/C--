a44b228289fff319523f12c6e1e8d1aef2409b95
bimmer <tarikkucevic@gmail.com>
2022-03-11 14:23:13 +0100

    working

---
+++ DSA/skracivanje-matrice-drugi-nacin.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int m, n, brojElemenata =0;
+    cout <<"unesite m i n: ";
+    cin >> m >>n;
+    int  matrix[m][n];
+    //posebno :
+    int r[n], v[n], c[n];
+    for(int i=0; i<m; i++){
+        for(int j = 0; j<n; j++){
+            cin >> matrix[i][j];
+            if(matrix[i][j] != 0){
+                r[brojElemenata] = i; 
+                c[brojElemenata] = j; 
+                v[brojElemenata] = matrix[i][j];
+                cout << "Trenutno  ubacijem : " << matrix[i][j] << "i on se nalazi u " << i << "vrsti " << "I " << j << "Koloni. \n";
+                brojElemenata++;
+            }
+        }
+    }
+    cout << " \n R: ";
+    for(int i = 0; i<brojElemenata; i++){
+        cout << r[i] << " ";
+    }
+    cout << " \n C: ";
+    for(int i = 0; i<brojElemenata; i++){
+        cout << c[i] << " ";
+    }
+        cout << " \n V: ";
+    for(int i = 0; i<brojElemenata; i++){
+        cout << v[i] <<" ";
+    }
+    return 0;
+}
