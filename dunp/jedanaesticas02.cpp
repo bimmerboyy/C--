2f0c6cdd7f40ffd86db4902dc7ca9f5f05e8252d
bimmer <tarikkucevic@gmail.com>
2022-03-16 10:48:58 +0100

    changed DUNP

--- dunp/jedanaesticas02.cpp
+++ dunp/jedanaesticas02.cpp
@@ -1,4 +1,7 @@
 /*
-Kreirati klasu figura koja im atribute oznaka,red i kolona
-
+Kreirati klasu figura koja im atribute oznaka,red i kolona metode konstruktoe sa i bez argumenata
+metodu proveri(Figuar f) koja proverava da li se dve figuer 
+nalaze na istom polju 
+metodu pojedi(Figura f) koja vraca poruku da li poruka moze da metodu pojedi (Figura f)
+koja vraca poruku da li figura moze da pojede drugu figuru F (da li se nalaze na sitoj poziciji)
 */
