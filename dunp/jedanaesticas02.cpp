02ef5b96da7831f876ddfd953f11d74d0d9cb81b
bimmer <tarikkucevic@gmail.com>
2022-03-16 20:15:13 +0100

    pointers exercise

--- dunp/jedanaesticas02.cpp
+++ dunp/jedanaesticas02.cpp
@@ -5,3 +5,20 @@ nalaze na istom polju
 metodu pojedi(Figura f) koja vraca poruku da li poruka moze da metodu pojedi (Figura f)
 koja vraca poruku da li figura moze da pojede drugu figuru F (da li se nalaze na sitoj poziciji)
 */
+#include<iostream>
+#include<string.h>
+using namespace std;
+/*
+class Figura(){
+    private:
+    int oznaka;
+    public:
+    void pojedi(Figura f){
+        if(boja == "BELA"){
+            if(vrsta == f.red && Math.fabs(kolona - f.kolona) == 1){
+                cout<<"Mozete da jedete figuru"<<endl;
+            }
+        }
+    }
+}
+*/
