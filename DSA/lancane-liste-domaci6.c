adf5825520ba0416247266b8d8b499c411eb61a1
anonymousvegan <majjunjulavgust@gmail.com>
2022-03-20 20:18:55 +0100

    zadatak 6 urađen

---
+++ DSA/lancane-liste-domaci6.c
@@ -0,0 +1,72 @@
+/*Zadatak 6. Napisati rekurzivni potprogram koji nalazi:
+(a) maksimalni element lančane liste,
+*/
+/*Napisati potprogram koji na osnovu zadate vrednosti elementa:
+(b) vraća element ako je nađen u listi
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+typedef struct node{
+    int value;
+    struct node* next;
+}NODE;
+
+
+
+NODE *first = NULL, *last=NULL, *temp=NULL;
+
+int isEmpty(){
+    if(first == NULL){
+        return 1;
+    }
+    return 0;
+}
+
+void ubaci(int a){
+    temp = (struct node*)malloc(sizeof(struct node));
+    temp -> value = a;
+    temp -> next = NULL;
+    if(isEmpty()){
+        last = temp;
+        first = temp;
+    }
+    else{
+        last -> next = temp;
+        last = temp;
+    }
+}
+
+void ispis(){
+    temp = first;
+    while(temp != NULL){
+        printf("%d \n",  temp->value);
+        temp = temp->next;
+    }
+}
+
+int najveci(node* f){
+    int max  = f -> value;
+    int t;
+    if(f->next != NULL){
+        t = najveci(f -> next);
+    }
+    if(t > max ){
+        max = t;
+    }
+    return max;
+}
+
+
+int main() {
+    ubaci(5);
+    ubaci(6);
+    ubaci(8);
+    ubaci(11);
+    ubaci(4);
+    int max = najveci(first);
+    printf("%d", max);
+    return 0;
+}
