51eecee12eb9c3a0e00904dce6172379d6f2720a
bimmer <tarikkucevic@gmail.com>
2022-03-29 18:05:52 +0200

    start homework

---
+++ dunp/domaci03.cpp
@@ -0,0 +1,24 @@
+/*
+Kreirati klasu figura koja im atribute oznaka,red i kolona metode konstruktoe sa i bez argumenata
+metodu proveri(Figuar f) koja proverava da li se dve figuer 
+nalaze na istom polju 
+metodu pojedi(Figura f) koja vraca poruku da li poruka moze da metodu pojedi (Figura f)
+koja vraca poruku da li figura moze da pojede drugu figuru F (da li se nalaze na sitoj poziciji)
+*/
+#include<iostream>
+#include<string.h>
+using namespace std;
+/*
+class Figura(){
+    private:
+    int oznaka;
+    public:
+    void pojedi(Figura f){
+        if(boja == "BELA"){
+            if(vrsta == f.red && Math.fabs(kolona - f.kolona) == 1){
+                cout<<"Mozete da jedete figuru"<<endl;
+            }
+        }
+    }
+}
+*/
