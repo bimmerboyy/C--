0ccef5f2ee8b2a8b22698960bec596d77988f937
anonymousvegan <majjunjulavgust@gmail.com>
2022-04-06 19:08:23 +0200

    implemented insert and delete funcitons to linked list

--- DSA/redovi2.c
+++ DSA/redovi2.c
@@ -4,6 +4,52 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+typedef struct node {
+    int data;
+    struct node* link;
+}NODE;
+
+NODE *front, *rear;
+
+void insert(int elem){
+    NODE* temp = (NODE*) malloc(sizeof(NODE));
+    if(temp==NULL){
+        printf("out of memory");
+        return;
+    }else{
+        temp -> data = elem;
+        temp -> link = NULL;
+        if(front == NULL){
+            front = rear = temp;
+        }
+        else{
+            rear-> link = temp;
+            rear = temp;
+            printf("Node has been inserted on the end successfully")
+        }
+    }
+}
+
+int delete(){
+    int elem; 
+    NODE *t;
+    if(front == NULL){
+        printf("Underflow");
+    }
+    else{
+        t = front;
+        elem = t -> data;
+        if(front == rear){
+            rear == NULL;
+        }
+        front = front -> link ;
+        t->link = NULL;
+        free(t);
+        return(elem);
+    }
+}
+
+
 int main(){
     return 0;
 }
