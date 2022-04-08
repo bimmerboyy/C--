f1ecd0ad5feb3c54236b20bc64ad253a1ef135e2
bimmer <tarikkucevic@gmail.com>
2022-04-08 15:32:14 +0200

    dunp

--- dunp/cetrnaesticas02.cpp
+++ dunp/cetrnaesticas02.cpp
@@ -29,7 +29,7 @@ class Ocena{
     }
     void setIP(char *ip){ strcpy(ime_predmeta,ip);}
     void setO(int o){ ocena = (o > 5 && o < 11) ? o : 5;}
-    void setO(char *d){ strcpy(datum,d);}
+    void setD(char *d){ strcpy(datum,d);}
     char *getIP(){return ime_predmeta;}
     int getO(){return ocena;}
     char* getD(){return datum;}
@@ -39,18 +39,19 @@ class Student{
     char broj_indeksa[10];
     Ocena ocena1,ocena2,ocena3;
     public:
-    Sudent(){
+    Student(){
         strcpy(ime_prezime, "Ela curic");
         strcpy(broj_indeksa, "036-010/21");
+        ocena1.setIP("OOP1");
         ocena2.setIP("Osnovi racunarstva");
         ocena2.setD("19.1.2022");
         ocena2.setO(7);
         ocena3.setIP("Osnovi programiranja");
         ocena3.setD("5.4.2022");
-        ocena3.setD(6);
-
+        ocena3.setO(6);
     }
-        Sudent(char *ip,char *bi,Ocena o1,Ocena o2,Ocena o3){
+        
+        Student(char *ip,char *bi,Ocena o1,Ocena o2,Ocena o3){
         strcpy(ime_prezime,ip);
         strcpy(broj_indeksa, bi);
         ocena1.setIP(o1.getIP());
@@ -67,6 +68,32 @@ class Student{
         int br = 0;
         if(ocena1.jeLiPOlozio())
         br++;
-        if()
+        if(ocena2.jeLiPOlozio())
+        br++;
+        br = (ocena3.jeLiPOlozio()) ? br+1 : br;
+        return br;
+    }
+    float prosek(){
+        int suma = 0;
+        if(ocena1.jeLiPOlozio())
+            suma += ocena1.getO();
+        if(ocena2.jeLiPOlozio())
+            suma += ocena2.getO();
+        if(ocena3.jeLiPOlozio())
+            suma += ocena3.getO();
+        return (float) suma / ukupnoPolozenih();
+    }
+    void ispis(){
+        cout<<"Student "<<ime_prezime<<" sa brojem indeksa "<<broj_indeksa<<" ima ocene "<<endl;
+        ocena1.ispis();
+        ocena2.ispis();
+        ocena3.ispis();
     }
 };
+int main(){
+    Student ela;
+    ela.ispis();
+    cout<<"Student je polozio "<<ela.ukupnoPolozenih()<<" predmeta"<<endl;
+    cout<<"Prosek studenta je "<<ela.prosek()<<endl;
+    return 0;
+}
