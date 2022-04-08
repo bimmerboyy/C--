1389c84cea4d643cccc90eba7b14e5e5b1768480
bimmer <tarikkucevic@gmail.com>
2022-04-08 17:54:53 +0200

    rastojanje tacaka

--- classes/classes20.cpp
+++ classes/classes20.cpp
@@ -31,10 +31,13 @@ class Tacka{
         return sqrt(pow(x-t.x,2) + pow(y-t.y,2));
     }
 };
+/*
 int main(){
     Tacka t1;
-    t1.postavi()
+    t1.aps();
+    t1.ord();
     t1.citaj();
     t1.pisi();
-    t1.rastojanje(5);
+    t1.rastojanje();
 }
+*/
