66625805a9a4cacfc0133dab339853534d541c54
bimmer <tarikkucevic@gmail.com>
2022-04-09 17:57:01 +0200

    pravimo ocene

--- vezbanje/studenti.cpp
+++ vezbanje/studenti.cpp
@@ -6,6 +6,7 @@ klasu ocene i formirace ocene za 3 predmeta.Imace moetde:ukuono polozenih,prosek
 
 #include<iostream>
 #include<string.h>
+using namespace std;
 
 class Ocena{
     private:
@@ -47,5 +48,26 @@ class Ocena{
     }
     int getO(){
         return ocena;
+    }
+};
+
+class Student{
+    private:
+    char ime_prezime[30];
+    int broj_indeksa;
+    Ocena ocena1,ocena2,ocena3;
+    public:
+    Student(){
+        strcpy(ime_prezime,"Tarik Kucevic");
+        broj_indeksa = 0360/17/2021;
+        ocena1.setO(8);
+        ocena1.setIP("OOP");
+        ocena1.setD("29.03.2022");
+        ocena2.setO(7);
+        ocena2.setIP("Elektrotehnika");
+        ocena2.setD("13.07.2022");
+        ocena3.setO(9);
+        ocena3.setIP("Linearna");
+        ocena3.setD("04.09.2022");
     }
 };
