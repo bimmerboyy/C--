3a9c354d61a276b335b16dee6df5404cab7e7886
bimmer <tarikkucevic@gmail.com>
2022-04-10 12:54:19 +0200

    redovi vezbanje

--- DSA/redovi-vezbanje01.c
+++ DSA/redovi-vezbanje01.c
@@ -51,5 +51,41 @@ int display(){
     if(isRedempty()){
         return -1;
     }
-    
+    int i;
+    for(i = f;i != r;(i+1)%SIZE){
+        printf("%d",red[i]);
+    }
+}
+int main(){
+    int opcija,elem,i,s[SIZE];
+    printf("1.Dodavanje elemenata 2.Brisanje elemenata 3.Display 4.Exit\n");
+    printf("Unesite opciju:");
+    scanf("%d", &opcija);
+    do{
+        switch(opcija){
+            case 1:
+            
+            printf("Unesite element:");
+            scanf("%d",&elem);
+            break;
+            case 2:
+            elem = brisanjeElizReda();
+            if(elem != -1){
+                printf("Izbrisan je %d",elem);
+            }
+            break;
+            case 3:
+            display();
+            break;
+            case 4:
+            printf("Exit");
+            break;
+            default:
+            printf("Uneta je pogresna opcija");
+            break;
+        }
+
+    }
+    while(opcija != 4);
+    return 0;
 }
