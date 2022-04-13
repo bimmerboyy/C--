f4dd31888507776fd961f28ce9c43ed0682df06b
bimmer <tarikkucevic@gmail.com>
2022-04-13 14:11:54 +0200

    redovi vezbanje

---
+++ DSA/redovi-vezbanje03.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+# define SIZE 100
+
+int f = -1;
+int r = -1;
+int red[SIZE];
+
+void insert_r(int elem){
+    if(f == r+1 % SIZE){
+        return;
+    }
+    if(f == r){
+        f = r = 0;
+        red[r] = elem;
+        return;
+    }
+    r = (r+1)%SIZE;  
+    red[r] = elem;
+}
+void insert_f(int elem){
+    if(f == (r+1)%SIZE){
+        
+
+    }
+}
