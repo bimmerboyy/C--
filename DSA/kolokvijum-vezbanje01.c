1b20278385544e4cbb196dd8606c00054b0e85c5
bimmer <tarikkucevic@gmail.com>
2022-04-18 15:28:19 +0200

    linked list exercise for colloquium

---
+++ DSA/kolokvijum-vezbanje01.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+typedef struct node{
+    int data;
+    struct node* next;
+}NODE;
+
+NODE *first = NULL,*last = NULL,*temp;
+
+bool isEmpty(){
+    if(first == NULL){
+        return true;
+    }
+    return false;
+}
+
+void unos(int elem){
+    temp = (struct node*)malloc(sizeof(struct node));
+    temp->data = elem;
+    temp->next = NULL;
+    if(isEmpty()){
+        first = temp;
+        last = temp;
+    }
+    else{
+        last->next = temp;
+        temp = last;
+    }
+}
+void display(){
+    if(isEmpty()){
+        printf("Lista je prazna");
+        return;
+    }
+    while(temp != NULL){
+        printf("%d",temp->data);
+        temp = temp->next;
+    }
+}
+void posleZadatog(){
+    
+}
