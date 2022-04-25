2d732c8cdd4ad15eff7d1703d92d17247011d8ff
bimmer <tarikkucevic@gmail.com>
2022-04-25 19:39:18 +0200

    ok

---
+++ vezbanje/nikola01.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"*"<<endl<<" "<<"*"<<"*"<<" "<<endl;
+    cout<<"*"<<endl<<"*"<<endl<<"*"<<endl<<"*"<<endl<<"*"<<endl<<"*"<<endl<<"*"<<endl<<"*"<<endl<<"*";
+
+    return 0;
+}s
