a5dd7390d9ea237e09460cead39c0bf28203066d
bimmer <tarikkucevic@gmail.com>
2022-04-26 12:56:31 +0200

    izvodjac

--- classes/classes25.cpp
+++ classes/classes25.cpp
@@ -14,6 +14,7 @@ a zatim se pojedinačni izvođači ispisuju u zasebnim redovima.*/
 
 #include<iostream>
 #include<string>
+#include<stdlib.h>
 using namespace std;
 
 enum Zanr {POP,REP,ROK};
@@ -29,6 +30,22 @@ public:
         this->zanr = zanr;
 
     }
+    string nadjiZanr(Zanr zanr){
+        switch(zanr){
+            case POP:
+            return "POP";
+            break;
+            case REP:
+            return "REP";
+            break;
+            case ROK:
+            return "ROK";
+            break;
+            default: 
+            return "GRESKA";
+        }
+    }
+    
    string getIme(){
        return imeIzvodjaca;
    }
@@ -36,20 +53,11 @@ public:
        return zanr;
    }
    void ispis(){
-       cout<<imeIzvodjaca<<"("<<zanr<<")"<<endl;
+       cout<<imeIzvodjaca<<"("<<nadjiZanr(zanr)<<")"<<endl;
    }
 
     
 };
-/*
-Klasa Pesma ima privatne atribute naziv,
-trajanje izraženo brojem minuta i brojem sekundi i maksimalni broj izvođača.
-Svi podaci se zadaju prilikom stvaranja kroz konstruktor sa argumentina(naziv, minuta, sekundi, brIzvodjaca).
-Može da se dohvati broj minuta i broj sekundi trajanja pesme ali ne mogu da se menjaju.
-Pomoću prijateljske funkcije može da se proveri koja od dve zadate pesme traje duže. 
-Metoda za dodavanje pojedinačnog izvođača (dodavanje ne uspeva u slučaju prekoračenja kapaciteta).
-Metoda ispis koja na glavnom izlazu ispisuje u obliku: P(naziv – minuti : sekundi),
-a zatim se pojedinačni izvođači ispisuju u zasebnim redovima.*/
 
 class Pesma{
 private:
@@ -58,12 +66,15 @@ private:
     int maksimalniBrIzvodjaca;
     Izvodjac *izv;
 public:
-    Pesma(string nazivPesme,int min,int sek,int maksmalniBrIzvodjaca){
-        this->nazivPesme = nazivPesme;
+    Pesma(int min,int sek,string nazivPesme,int maksmalniBrIzvodjaca){
         this->min = min;
         this->sek = sek;
+        this->nazivPesme = nazivPesme;
         this->maksimalniBrIzvodjaca = maksmalniBrIzvodjaca;
+        this->brIzvodjaca = 0;
+        izv = new Izvodjac[maksmalniBrIzvodjaca];
     }
+
     
     int getMin(){
         return min;
@@ -83,13 +94,13 @@ public:
         }
     }
     void dodajIzvodjaca(Izvodjac i){
+
         if(brIzvodjaca < maksimalniBrIzvodjaca){
             izv[brIzvodjaca] = i;
             brIzvodjaca++;
         }
     }
-     /*P(naziv – minuti : sekundi),
-a zatim se pojedinačni izvođači ispisuju u zasebnim redovima.*/
+     
     void ispis(){
         cout<<"P"<<"("<<nazivPesme<<" - "<<min<<" : "<<sek<<endl;
         for(int i = 0;i < brIzvodjaca;i++){
@@ -98,3 +109,26 @@ a zatim se pojedinačni izvođači ispisuju u zasebnim redovima.*/
         
     }
 };
+int main(){
+    Izvodjac i1("Sukro Sukrovic",POP);
+    Izvodjac i2("Roki Role",REP);
+    Izvodjac i3("Zeki Zeko",ROK);
+
+    Pesma p1(3,45,"OK ti",2);
+    Pesma p2(2,25,"Brko",2);
+    Pesma p3(1,50,"Cigan sam",3);
+
+    p1.dodajIzvodjaca(i1);
+    p1.dodajIzvodjaca(i2);
+    p2.dodajIzvodjaca(i1);
+    p2.dodajIzvodjaca(i2);
+    p2.dodajIzvodjaca(i3);
+    p3.dodajIzvodjaca(i3);
+    p3.dodajIzvodjaca(i3);
+
+    p1.ispis();
+    p2.ispis();
+    p3.ispis();
+    return 0;
+
+}
