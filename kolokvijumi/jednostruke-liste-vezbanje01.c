351a0f32e355ffe38ffbb8a35ace98e35baed841
bimmer <tarikkucevic@gmail.com>
2022-05-05 23:30:42 +0200

    liste vezbanje

---
+++ kolokvijumi/jednostruke-liste-vezbanje01.c
@@ -0,0 +1,67 @@
+/*Zadatak 1. Napisati potprogram koji na osnovu zadate vrednosti elementa:
+(a)vraća 1 ako se elementa nalazi u lančanoj listi, u suprotnom vraća 0
+(b) vraća element ako je nađen u listi
+(c) vraća presek dve lančane liste
+
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+typedef struct node{
+    int data;
+    struct node *next;
+}NODE;
+
+NODE *first = NULL,*last = NULL,*temp;
+
+bool isEmpty(){
+    return first = NULL;
+}
+
+void unos(int elem){
+    temp = (struct node*)malloc(sizeof(struct node));
+    temp->data = elem;
+    temp->next = NULL;
+    if(isEmpty()){
+        first = temp;
+        last = temp;
+    }
+    last->next = temp;
+    last = temp;
+}
+
+void dipslay(){
+    if(isEmpty()){
+        printf("Lista je prazna");
+    }
+    temp = first;
+    while(temp != NULL){
+        printf("%d",temp->data);
+        temp = temp->next;
+    }
+}
+
+/*
+void trazeniElement(int trazeniElem){
+    temp = first;
+    while(temp != NULL){
+        if(temp->data == trazeniElem){
+            printf("Pronasao sam trazeni element i on je: %d",trazeniElem);
+            temp = temp->next;
+        }
+
+    }
+}
+*/
+
+int main(){
+    unos(1);
+    unos(2);
+    unos(3);
+    unos(4);
+    unos(5);
+    dipslay();
+    return 0;
+}
