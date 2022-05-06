9cb923cb824c4f445a74c1806de89bf9246561aa
anonymousvegan <majjunjulavgust@gmail.com>
2022-05-06 20:53:19 +0200

    napravljene dvostruko ulančane

--- vezbanje-za-prvi-kolokvijum-dsa/dvostruko-ulancane-liste.c
+++ vezbanje-za-prvi-kolokvijum-dsa/dvostruko-ulancane-liste.c
@@ -11,39 +11,75 @@ typedef struct node{
 NODE *first, *temp;
 
 struct node* create_new_node(int x){
-    temp = (struct node*)malloc(sizeof(struct node));
+    struct node *new_node = (struct node*)malloc(sizeof(struct node));
     new_node -> data = x;
     new_node -> next = NULL;
-    new_node -> prew = NULL;
+    new_node -> prev = NULL;
     return new_node;
 }
 
 void insert_at_head(int x){
     temp = create_new_node(x);
-    if(head == NULL){
-        head = temp;
+    if(first == NULL){
+        first = temp;
         return; 
     }
-    head -> prew = temp;
-    temp -> next = head;
-    head = temp;
+    first -> prev = temp;
+    temp -> next = first;
+    first = temp;
 }
 
 void insert_at_tail(int x){
-    temp = head;
+    temp = first;
     struct node* new_node = create_new_node(x);
-    if(head == NULL){
-        head = new_node;
+    if(first == NULL){
+        first = new_node;
         return;
     }
-    while(temp != NULL){
-        temp = temp-> next;
-    }
-    new_node -> prew = temp;
+    while(temp -> next != NULL){
+        temp = temp -> next;
+    } 
     temp -> next = new_node;
+    new_node -> prev = temp;
 }
 
-int main(){
+//ispisivanje
 
+void display(){
+    temp = first;
+    while(temp != NULL){
+        printf("%d \n", temp->data);
+        temp = temp->next;
+    }
+}
+
+void display_reverse(){
+    temp = first;
+    while(temp -> next != NULL){
+        temp = temp -> next;
+    }
+    while(temp != NULL){
+        printf("%d \n", temp->data);
+        temp = temp -> prev ;
+    }
+}
+
+void delete_actual(struct node *pom){
+    
+}
+
+
+
+int main(){
+    insert_at_head(3);
+    insert_at_head(4);
+    insert_at_head(5);
+    insert_at_tail(6);
+    insert_at_tail(7);
+    insert_at_tail(8);
+    printf("normal:  \n");
+    display();
+    printf("reverse: \n");
+    display_reverse();
     return 0;
 }
