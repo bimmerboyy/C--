df4daf17685e2d2672bcdb3021156579f3a8af5e
bimmer <tarikkucevic@gmail.com>
2022-05-07 16:09:05 +0200

    kruzne liste vezbanje

---
+++ kolokvijumi/jednostruke-liste-vezbanje04.c
@@ -0,0 +1,23 @@
+//Zadatak 5. Napisati potprogram kojim se proverava da li su dve lančane liste L1 i L2 jednake.
+
+#include<stdio.h>
+#include <stdlib.h>
+#include<stdbool.h>
+
+typedef struct node{
+    int data;
+    struct node *next;
+}NODE;
+
+NODE *first1,*last1,*temp;
+NODE *first2,*last2;
+
+void unesi1(){
+    temp = (struct node*)malloc(sizeof(struct node));
+    
+}
+
+int main(){
+
+    return 0;
+}
