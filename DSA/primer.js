8d27051230646a3e0a4cf4cb33cb7376bb25a47b
anonymousvegan <majjunjulavgust@gmail.com>
2022-05-15 12:19:17 +0200

    primer

---
+++ DSA/primer.js
@@ -0,0 +1,8 @@
+let osoba = {
+    ime: "Nikola",
+    prezime: "Maktović",
+    godine : 2002,
+    ima_devojku: true,
+    normalan : false,
+    prijatelji: ["tarik", "ermin", "bahir"]
+};
---
+++ DSA/stabla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct stablo{
+    int data;
+    struct stablo *left, *right;
+}STABLO;
+
+struct stablo *koren = NULL;
+
+int main(){
+
+    return 0;
+}
