6c8b8a7e0ea109c7e0ade568592d1ab1d29806e4
bimmer <tarikkucevic@gmail.com>
2022-05-18 08:34:08 +0200

    dunp

--- dunp/dvadesetdrugicas02.cpp
+++ dunp/dvadesetdrugicas02.cpp
@@ -2,3 +2,28 @@
 
 using namespace std;
 
+class Linearna{
+    protected:
+    float b,c;
+    public:
+    Linearna(float b = 2,float c = 3){
+        this->b = b;
+        this->c = c;
+    }
+    virtual void  resenje(){
+        if(c != 0){
+            float x = -b/c;
+            cout<<"Jednacina ima resenje x = "<<x<<endl;
+        }
+        else{
+            cout<<"Jednacina nema resenja jer je c = 0"<<endl;
+        }
+    }
+    virtual void ispis(){
+        cout<<"Linearna jednacina je oblima "<<b<<","<<c<<" = 0"<<endl;
+    }
+};
+
+class Kvadratna{
+
+};
