b1cea414b12401fcba2fd1566cf459c4cbcfa396
anonymousvegan <majjunjulavgust@gmail.com>
2022-05-18 21:15:44 +0200

    gotovo za danas

--- vezbanje/nasledjivanje2.cpp
+++ vezbanje/nasledjivanje2.cpp
@@ -13,7 +13,7 @@ class Kvadar{
     protected:
     int a,b,c;
     public:
-    Kvadar(int a, int b, int c){
+    Kvadar(){
         a = 1;
         b = 1;
         c = 1;
@@ -31,23 +31,19 @@ class Bazen : public Kvadar {
     protected:
     int d;
     public:
-    Bazen() : Kvadar(){
-        this->d = 2;
-    }
-    Bazen(int a = 1,int b = 1,int c = 1, int d = 1) : Kvadar(a,b,c){
+    Bazen(int d) : Kvadar(){
         this->d = d;
     }
+    // Bazen(int a = 1,int b = 1,int c = 1, int d = 1) : Kvadar(a,b,c){
+    //     this->d = d;
+    // }
     void ispis(){
         cout << "a: " << a << ", b:" << b << ", c: " << c << ", d: "<<d<< endl;
     }
 };
 
 int main(){
-    Kvadar k1(1,2,3), k2;
-    Bazen  b1(1,2,3,2), b2;
-    k1.ispis();
-    k2.ispis();
+    Bazen  b1(2);
     b1.ispis();
-    b2.ispis();
     return 0;
 }
