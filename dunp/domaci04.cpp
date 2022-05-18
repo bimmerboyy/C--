6e09f3d9a5faba3b9c1b5f49f5ed0ecd02377220
bimmer <tarikkucevic@gmail.com>
2022-05-18 12:04:53 +0200

    dunp

---
+++ dunp/domaci04.cpp
@@ -0,0 +1,7 @@
+/* Primer klase Student, RedovanStudent i VandredniStudent RedovanStudent placa ukupnu skolarinu kao isvaki student 
+VandrednStudent placa ukupnu skolarinu tako sto se doda jos odredjeni iznos
+voditi spisak svih uplata do sada evidentiranih
+
+ukupnaZaradafakulteta(Studenata *s)
+
+*/
