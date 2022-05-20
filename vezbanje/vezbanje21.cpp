f589d18e4b4676a465e791fc29e1c7d4434a6ffd
bimmer <tarikkucevic@gmail.com>
2022-05-20 19:28:08 +0200

    vezbanje

---
+++ vezbanje/vezbanje21.cpp
@@ -0,0 +1,26 @@
+/*U sledećem primeru se kreira klasa Pravougaonik. Ova klasa poseduje konstruktore, atribute,
+propertije i javne metode za računanje obima i površine. Iz ove klase je primenom nasleđivanja izvedena
+klasa Kvadrat. Prilikom nasleđivanja se ne nasleđuju konstruktori, pa klasa Kvadrat mora da
+implementira svoje sopstvene konstruktore. Ostali elementi se nasleđuju. Na kraju se iz main metode
+kreiraju objekti za kreirane klase i nad njima se pozivaju metode kako bi se dobio željeni ispis. */
+
+#include<iostream>
+
+using namespace std;
+
+class Pravougaonik{
+    protected:
+    int a;
+    int b;
+    public:
+    Pravougaonik(int a, int b){
+        this->a = a;
+        this->b = b;
+    }
+    
+};
+
+int main(){
+
+    return 0;
+}
