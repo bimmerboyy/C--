5e79473267fb200a030d43b59a9391f1786d777a
bimmer <tarikkucevic@gmail.com>
2022-05-22 18:12:23 +0200

    quick sort

---
+++ vezbanje/quick-sort.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int i,n;
+    cout<<"Unesite n:";
+    cin >> n;
+    int niz[n];
+
+    for(i = 0;i < n;i++){
+        cin>>niz[i];
+    }
+    cout<<"Niz pre QuickSort: ";
+    for(i = 0;i < n;i++){
+        cout<<niz[i]<<" ";
+    }
+    cout<<endl<<"Niz posle quick sorta"<<endl;
+
+    int f,l;
+    f = 0;
+    l = n-1;
+    for(i = 0;i < n;i++){
+        
+    }
+
+
+    return 0;
+}
