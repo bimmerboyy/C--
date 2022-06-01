1829759223c559588157841a3b44c791a7315a7d
anonymousvegan <majjunjulavgust@gmail.com>
2022-06-01 20:02:35 +0200

    urađen drugi deo zadatka

---
+++ ispiti-vezbanje/avgust-2018.cpp
@@ -0,0 +1,132 @@
+// Realizovati sledeće klase :
+// (klase opremiti onim konstruktorima, destruktorima i operatorima dodele (preklopljenim operatorima)
+// koji su potrebni za bezbedno i efikasno korišćenje.
+// 1. Nalepnica ima naziv i jednoznačni automatski generisan identifikator kojem može da se pristupi. 
+//     Naziv se zadaje sa standardnog ulaza prilikom stvaranje objekta.
+//     Prilikom kopiranja voditi računa da se kopira naziv a da se identifikator sam kreira. 
+//     Omogućiti metodu za ispis. 
+// 2. Flaša pića ima sledeće podatke: 
+//     nalepnicu, cenu bez kaucije (u koju nije uračunata cena flaše), 
+//     zapreminu (podrazumevano 0,33 litara) i vrstu pića. 
+//     Vrsta pića može biti BEZALKOHOLNO (podrazumevano) i ALKOHOLNO.
+//     Svi podaci se unose prilikom stvaranja objekta i mogu se odštampati, a samo se cena može promeniti.
+//     Kreirati metodu koja može da izračuna cenu flaše pića sa kaucojom
+//     (ima razlike za staklenu i plastičnu flašu).
+//     Metodu PROVERA koja proverava da li su dve flaše jednake (flaše su jednake ukoliko imaju isti naziv nalepnice,
+//     cenu flaše sa kaucijom i vrstu pića. Omogućiti metodu za ispis.
+
+// 3. Staklena flaša pića je flaša pića kod koje se cena sa kaucijom računa tako što se na cenu bez kaucije doda 5% ukoliko
+//     je zapremina manja od 0,5 litara ili 10% ukoliko je zapremina veća ili jednaka od 0,5 litara.
+//     Preklopiti metodu ispis da ispisuje podatke o flaši i poruku da je flaša staklena.
+// 4. Plastična flaša pića je flaša pića kod koje je cena sa kaucijom jednaka ceni bez kaucije. Preklopiti metodu za ispis da 
+//     ispisuje podatke o flaši i poruku da je flaša plastnična.
+// 5. Formirati funkciju UKUPNO, van klasa koja formira dinamički niz staklenih i plastičnih flaša
+//     čije podatke unosi korisnik sa tastature i zatim računa i vraća ukupan zbir svih flaša sa kaucijom.
+// U main funkciji napraviti po jedan objekat svake klase i testirati sve metode, kao i funkciju UKUPNO.
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+enum Vrsta {BEZALKOHOLNO, ALKOHOLNO};
+
+class Nalepnica {
+    protected:
+    string naziv;
+    int identifikator;
+    static int brojac;
+    public:
+    Nalepnica(){
+        cout << "Unesite naziv:";
+        cin >> naziv;
+        identifikator = brojac;
+        brojac++;
+    }
+    Nalepnica(const Nalepnica& n1){
+        this->naziv = n1.naziv;
+        this->identifikator = brojac;
+        brojac++;
+    }
+    int get_identifikator(){
+        return identifikator;
+    }
+    string get_naziv(){
+        return naziv;
+    }
+    friend ostream& operator<<(ostream& out, Nalepnica &n1){
+        out << "Naziv: " << n1.naziv << " id: " << n1.identifikator << endl; 
+        return out;
+    }
+};
+
+class Flasa{
+    protected:
+    Nalepnica nalepnica;
+    float cena_bez_kaucije;
+    float zapremina;
+    Vrsta vrsta;
+    public:
+    Flasa(float cena_bez_kaucije, float zapremina = 0.33, Vrsta vrsta = BEZALKOHOLNO ){
+        this-> cena_bez_kaucije = cena_bez_kaucije;
+        this-> zapremina = zapremina;
+        this-> vrsta = vrsta;
+    }
+    Nalepnica get_nalepnica(){
+        return nalepnica;
+    }
+    float get_cenabez_kaucije(){
+        return cena_bez_kaucije;
+    }
+    float get_zapremina(){
+        return zapremina;
+    }
+    string get_vrsta(){
+        switch(vrsta){
+            case 0:
+               return "Bezalkoholno";
+            case 1:
+                return "Alkoholno";
+        }
+        return "";
+    }
+    void set_cena_bez_kaucije(float cena_bez_kaucije){
+        this-> cena_bez_kaucije = cena_bez_kaucije;
+    }
+    virtual float cena_sa_kaucijom(float kaucija = 0 ){
+        return cena_bez_kaucije + (cena_bez_kaucije / 100 * kaucija);
+    }
+    friend bool da_li_su_jednake(Flasa f1, Flasa f2){
+        if(f1.nalepnica.get_naziv() == f2.nalepnica.get_naziv() && f1.cena_sa_kaucijom() == f2.cena_sa_kaucijom() && f1.vrsta == f2.vrsta){
+            return true;
+        }
+        return false;
+    }
+    bool operator  ==(Flasa &f1){
+        if(f1.nalepnica.get_naziv() == nalepnica.get_naziv() && f1.cena_sa_kaucijom() == cena_sa_kaucijom() && f1.vrsta == vrsta){
+            return true;
+        }
+        return false;
+    }
+    friend ostream& operator<<(ostream& out, Flasa f1){
+        out << f1.nalepnica;
+        out << "cena bez kaucije: " << f1.cena_bez_kaucije << ", Cena sa kaucijom " << f1.cena_sa_kaucijom() << endl;
+        out << "Zapemina : " << f1.zapremina  << "ml, vrsta: " << f1.get_vrsta(); 
+        return out;
+    }
+};
+
+
+
+int Nalepnica::brojac = 0;
+
+int main(){
+    Flasa f1(70, 0.5,  BEZALKOHOLNO);
+    Flasa f2(70, 0.5,  BEZALKOHOLNO);
+    Flasa f3(100, 0.5,  BEZALKOHOLNO);
+    Flasa f4(70,  0.5,  BEZALKOHOLNO);
+    if(f1 == f2){
+        cout << "f1 == f2" << endl;
+    }
+    cout << f1;
+    return 0;
+}
