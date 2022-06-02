f1ec1c1943f54aa436bb5265d1ed67dc889ca8a6
bimmer <tarikkucevic@gmail.com>
2022-06-02 11:14:00 +0200

    pointers

--- ispiti-vezbanje/pokazivaci02.c
+++ ispiti-vezbanje/pokazivaci02.c
@@ -12,7 +12,9 @@ int main(){
     printf("p pre brisanja = %d\n",*p);
     free(p);
     printf("p posle brisanja = %n\n",p);
+    p = (int*)malloc(sizeof(int));
     p = q;
+    printf("p = %d p = q\n",*p);
     q = (int*)malloc(sizeof(int));
     *q = 3;
     printf("p = %d, q = %d\n", *p, *q);
