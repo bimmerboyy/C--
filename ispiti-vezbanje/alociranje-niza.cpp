39ec6a38c22b4aba7a71e03fce05ffda7354aeb5
bimmer <tarikkucevic@gmail.com>
2022-06-03 21:33:40 +0200

    work with alocating array

--- ispiti-vezbanje/alociranje-niza.cpp
+++ ispiti-vezbanje/alociranje-niza.cpp
@@ -1,8 +1,24 @@
 #include<iostream>
+#include<stdlib.h>
 
 using namespace std;
 
+
+class Array{
+    private:
+    int duzinaNiza;
+    int *niz;
+    public:
+    Array(int duzinaNiza = 5){
+        niz = new int[ duzinaNiza];
+        for(int i = 0; i < duzinaNiza;i++){
+            
+        }
+        this->duzinaNiza = duzinaNiza;
+    }
+};
+
 int main(){
     return 0;
-    
+
 }
