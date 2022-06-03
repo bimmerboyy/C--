0fb3fb0bcd48519d1fb282767fcb72bc72a9bc80
bimmer <tarikkucevic@gmail.com>
2022-06-03 21:51:19 +0200

    work with alocating array

--- ispiti-vezbanje/alociranje-niza.cpp
+++ ispiti-vezbanje/alociranje-niza.cpp
@@ -4,25 +4,47 @@
 using namespace std;
 
 
-class Array{
+class Niz{
     private:
     int duzinaNiza;
     int *niz;
     public:
-    Array(int duzinaNiza = 5){
+    Niz(int duzinaNiza = 5){
         niz = new int[ duzinaNiza];
         for(int i = 0; i < duzinaNiza;i++){
             niz[i] = 0;
         }
         this->duzinaNiza = duzinaNiza;
     }
-    ~Array(){
+
+    void unosNiza(){
+        niz = new int[duzinaNiza];
+        cout<<"Unesite elemente niza:";
+        for(int i = 0; i < duzinaNiza;i++){
+            cin >> niz[i];
+        }
+    }
+   
+    friend ostream&  operator<<(ostream& COUT,Niz n1){
+        COUT<<"Niz je:"<<endl;
+        for(int i = 0; i < n1.duzinaNiza; i++){
+            COUT<<n1.niz[i]<<" ";
+        }
+        COUT<<endl;
+        return COUT;
+
+    }
+     ~Niz(){
         delete []niz;
         niz = nullptr;
+        cout<<"Obrisao sam niz pomocu destrutor"<<endl;
     }
 };
 
 int main(){
+    Niz n1;
+    n1.unosNiza();
+    cout <<n1;
     return 0;
 
 }
