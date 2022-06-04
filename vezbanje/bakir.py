9f97f4fad6f1d5ed483c78432316ce4e8accdb3c
bimmer <tarikkucevic@gmail.com>
2022-06-04 21:42:03 +0200

    text in pygame

---
+++ vezbanje/bakir.py
@@ -0,0 +1,21 @@
+import pygame
+import sys
+
+pygame.init()
+display = pygame.display.set_mode((400,300,))
+
+
+myfont = pygame.font.SysFont("verdana",20)
+
+mytext = myfont.render("Pygame font and text",True,(255,0,0))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()   
+            sys.exit()
+    display.fill((0,0,0))
+
+    display.blit(mytext,(100,120))
+
+    pygame.display.update()
