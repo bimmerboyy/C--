b5cc13d2c34a83da64476b5a99982f3ee544162f
anonymousvegan <majjunjulavgust@gmail.com>
2022-06-05 19:06:13 +0200

    buggs fixes

--- ispiti-vezbanje/septembar-2020.cpp
+++ ispiti-vezbanje/septembar-2020.cpp
@@ -75,11 +75,88 @@ class Sastojak{
     }
 };
 
+
+// 2.	
+//      Klasa Jelo ima ime i sadrži listu sastojaka, 
+//      kao i celobrojnu vrednost koja predstavlja količinu sastojka za pripremu jela izraženu u gramima.
+//      Cena jela je određena zbirom cene sastojaka,
+//      uz eventualno procentualno povećanje ili smanjenje osnovne cene,
+//      koje zavisi od sata formiranja cene jela. 
+//      Može da se postavi podatak o procentualnoj promeni cene jela i podatak o satu formiranja cene.
+//      Može da se doda sastojak određene količine u jelo, 
+//      pri čemu se pre dodavanja proverava da li je sastojak adekvatan(greška je ako nije),
+//      da se dohvati jednoslovna vrsta jela i da se izračuna njegova cena.
+//      Upisuje se u izlazni tok(cout<<j) tako što se u prvom redu upiše 
+//      ime:cena, a u narednim redovima se upisuje jedan sastojak poredu u obliku sastojak:količina. 
+//      Pri uništavanju jela, uništavaju se i svi sadržani sastojci.
+class Jelo{
+    protected:
+    string ime;
+    Sastojak * sastojci;
+    int * kolicine_sastojaka;
+    int trenutni_broj_sastojaka;
+    int kolicina;
+    public:
+    Jelo(string ime = "Torta"){
+        this->ime = ime;
+        trenutni_broj_sastojaka = 0;
+    }
+    void dodaj_sastojak(Sastojak s1, int kolicina){
+        cout << "ubacujem : " << s1;
+        Sastojak * pomocni = new Sastojak[trenutni_broj_sastojaka];
+        int * pomocni2 = new int[trenutni_broj_sastojaka];
+        for(int i = 0; i < trenutni_broj_sastojaka; i++){
+            pomocni[i] = sastojci[i];
+            pomocni2[i] = kolicine_sastojaka[i];
+        }
+        trenutni_broj_sastojaka++;
+        sastojci  = new Sastojak[trenutni_broj_sastojaka];
+        kolicine_sastojaka = new int[trenutni_broj_sastojaka];
+        for(int i = 0; i < trenutni_broj_sastojaka - 1; i++){
+            sastojci[i] = pomocni[i];
+            kolicine_sastojaka[i] = pomocni2[i];
+        }
+        sastojci[trenutni_broj_sastojaka-1] = s1;
+        kolicine_sastojaka[trenutni_broj_sastojaka - 1] = kolicina;
+    }
+    friend ostream& operator<<(ostream& out, Jelo j1){
+        out << j1.ime << ":" << "cena" << endl;
+        for(int i = 0; i < j1.trenutni_broj_sastojaka; i++){
+            out << j1.sastojci[i];
+        }
+        return out;
+    }
+    float cena_jela(){
+        float s = 0;
+        for(int i = 0; i < trenutni_broj_sastojaka; i++){
+            s += sastojci[i].cena(kolicine_sastojaka[i]);
+        }
+        return s;
+    }
+};
+
+
+
+
 int main(){
     Sastojak s1("Jagode", 200, SLADAK);
     cout << s1;
     cout << s1.cena(100);
     cout << s1.get_vrsta();
-
+    Sastojak zemicka("Zemicka", 300, SLAN);
+    Sastojak barbikju_sos("barbikjusos", 1000, NEUTRALNAN);
+    Sastojak salata("Salata", 100, NEUTRALNAN);
+    Sastojak paradajz("Paradajz", 100, NEUTRALNAN);
+    Sastojak cedar_sir("cedar_sir", 500, SLAN);
+    Sastojak meso("Meso", 700, SLAN);
+    Jelo burger("burger");
+    burger.dodaj_sastojak(zemicka, 100);
+    burger.dodaj_sastojak(barbikju_sos, 100);
+    burger.dodaj_sastojak(salata, 100);
+    burger.dodaj_sastojak(paradajz, 100);
+    burger.dodaj_sastojak(cedar_sir, 100);
+    burger.dodaj_sastojak(meso, 50);
+    cout <<burger << endl;
+    cout <<burger.cena_jela();
     return 0;
 }
