da0e60a44564bc726a84e0d49de2e84e9447d3a8
anonymousvegan <majjunjulavgust@gmail.com>
2022-06-08 16:00:31 +0200

    najveća težina u grafu

--- ispiti-vezbanje/graf-preko-matrice.c
+++ ispiti-vezbanje/graf-preko-matrice.c
@@ -27,6 +27,18 @@ int main(){
     int matrix[MAX][MAX]; 
     unos(matrix, n);
     ispis(matrix, n);
-    
+    printf(" Grane koje imaju vezu su: \n");
+    int max = matrix[0][0];
+    for(int i =  0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(matrix[i][j]  != 0){
+                printf("Grana %d ima vezu sa granom %d, težina je %d \n", i, j, matrix[i][j]);
+                if(matrix[i][j]>max){
+                    max = matrix[i][j];
+                }
+            }
+        }
+    }
+    printf("najveća težina je : %d \n", max);;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     return 0;
 }
