2ce2860643a9ffefbe5bf84e5996b05bb360873b
bimmer <tarikkucevic@gmail.com>
2022-06-10 15:52:56 +0200

    binary tree exercise

---
+++ ispiti-vezbanje/binarno-stablo-zadatak01.c
@@ -0,0 +1,21 @@
+// Zadatak 1. Napisati rekurzivnu funkciju u pseudokodu kojom se ispisuje sadržaj informacionih polja u listovima binarnog stabla.
+
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct tree{
+    int data;
+    struct tree *left;
+    struct tree *right;
+}TREE;
+
+TREE *root = NULL,*temp;
+
+void createNode(int elem){
+    
+}
+
+int main(){
+
+    return 0;
+}
