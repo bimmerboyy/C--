6292704f256a7836d47216cfec60a1d574930077
bimmer <tarikkucevic@gmail.com>
2022-06-13 15:56:50 +0200

    first exercise over

--- ispiti-vezbanje/2-kol-cpp.cpp
+++ ispiti-vezbanje/2-kol-cpp.cpp
@@ -2,9 +2,12 @@
 
 using namespace std;
 
-/*Tacka u ravni zadaje se kvadratnom (1 kvadrant,2 kvadrant,3 kvadrant ili 4 kvadrant), i realnim koordinatama x i y 
+/*1.Tacka u ravni zadaje se kvadratnom (1 kvadrant,2 kvadrant,3 kvadrant ili 4 kvadrant), i realnim koordinatama x i y 
 podrazumevano(0,0) koje mogu da se dohvate.Kvadranti sluze da bi se izvrsila provera da li koordinate tacke ispravno unete.
 Tacka moze da se upise u izlazini tok (cout<<tacka) u obliku (x,y)
+
+2.Apstraktna kriva u ravni predvidja stvaranje tacke  cije koordinate se racunaju na osnovu zadatog realnog parametra p(kriva(p)).
+Moze da se napravi kopija krive i da se kriva upise u izlazni tok (cout<<kriva)
 */
 
 class Tacka{
@@ -15,6 +18,12 @@ class Tacka{
     Tacka(int x,int y){
         this->x = x;
         this->y = y;
+    }
+    int getX(){
+        return x;
+    }
+    int getY(){
+        return y;
     }
     bool daLiSeNalazi(){
         if(x > 0 && y > 0){
@@ -38,6 +47,17 @@ class Tacka{
         COUT<<"("<<t1.x<<","<<t1.y<<")"<<endl;
         return COUT;
     }
+};
+
+class Kriva{
+    protected:
+    Tacka p;
+    public:
+    Kriva(Tacka p) : p(p){}
+    Kriva(Kriva& k1){
+
+    }
+
 };
 
 int main(){
