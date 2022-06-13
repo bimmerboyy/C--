b68115f700113a6d0b6510a3522fd295c8ffceb3
bimmer <tarikkucevic@gmail.com>
2022-06-13 20:09:21 +0200

    secon col is already finished

--- ispiti-vezbanje/2-kol-cpp.cpp
+++ ispiti-vezbanje/2-kol-cpp.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cmath>
 
 using namespace std;
 
@@ -48,8 +49,9 @@ class Tacka{
     }
     void setY(int y){
         this->y = y;
-       
     }
+    
+    
 
     bool daLiSeNalazi(){
         if(x > 0 && y > 0){
@@ -83,25 +85,106 @@ class Tacka{
 
 class Kriva{
     protected:
-    Tacka p,t;
+    Tacka t;
+    float p;
     public:
-    Kriva(Tacka x,Tacka y){
-        p = x;
-        t = y;
-               
+    Kriva(float p){
+        this->p = p;
+    
     }
+    Kriva(const Kriva& k1){
+        p = k1.p;
+        t = k1.t;
+    }
+ 
     Kriva(){
        
     }
-    // Kriva(Kriva& k1){
-    //     p = k1.p;
-    // }
+    Tacka getT(){
+        return t;
+    }
+   
+friend ostream& operator<<(ostream& os,Kriva &k1){
+        os<<"Kriva ima korde:"<<"("<<k1.p<<","<<k1.t.getX()<<","<<k1.t.getY()<<")"<<endl;
+        return os;
+
+    } 
+};
+
+
+class Spirala : Kriva{
+    protected:
+    float a;
+    float b;
+    public:
+    Spirala(float p,float a,float b) : Kriva(p){
+        this->a = a;
+        this->b = b;
+    }
+      Spirala(){
+        a = 1;
+        b = 0;
+    }
+    float izracunajX(){
+        t.setX(a * p * cos(b+p));
+        return t.getX();
+    }
+      float izracunajY(){
+        t.setX(a * p * sin(b+p));
+        return t.getX();
+    }
+    friend ostream& operator<<(ostream& os,Spirala &s1){
+        os<<"Spirala:"<<"["<<s1.izracunajX()<<","<<s1.izracunajY()<<"]"<<endl;
+        return os;
+    }
+};
+// 4.Prikazivac sadrzi krivu ka i tri realna parametra pMin,pMax DELTAp,koji se zadaju prilikom stvaranja.Moze da se zameni sadrzan kriva
+// da se promene sva tri parametra odjednom da se tabele sadrzane krive upise u zadati izlazni tok i da se prikazivac ispise u izlazni tok
+// (cout<<prik) .Tabela krive  se sastoji od zaglavlja i parova (p,k(p)) za pmin <= p <= pmax sa korakom DELTAp.Upisivanje prikazivaca u 
+// izalzni tok je oblika k(pmin,pmin,DELTAp) gde je k rezultat upisivanje sadrzajne krive 
+
+class Prikazivac{
+    private:
+    Kriva k1;
+    float pMin;
+    float pMax;
+    float DELTAp;
+    public:
+    Prikazivac(float pMin, float pMax, float DELTAp){
+        this->pMin = pMin;
+        this->pMax = pMax;
+        this->DELTAp = DELTAp;
+
+    }
+    void setAll(float pMin, float pMax, float DELTAp){
+        this->pMin = pMin;
+        this->pMax = pMax;
+        this->DELTAp = DELTAp;
+    }
+   void setKriva(Kriva k1){
+    this->k1 = k1;
+}
+    void tabela(){
+        for(int p = pMin; p < pMax; p+=DELTAp){
+            cout<<"("<<p<<","<<k1.getT().getX()<<","<<k1.getT().getY()<<")"<<endl;
+        }
+    }
+    friend ostream& operator<<(ostream& out,Prikazivac p1){
+        out<<"k"<<"("<<p1.pMin<<","<<p1.pMax<<","<<p1.DELTAp<<")"<<endl;
+        for(int p = p1.pMin; p < p1.pMax; p+=p1.DELTAp){
+            out<<"("<<p<<","<<p1.k1.getT().getX()<<","<<p1.k1.getT().getY()<<")"<<endl;
+        }
+    }
 
 };
+
 
 int main(){
     Tacka t1(0,0);
     t1.daLiSeNalazi();
+    Kriva k1(4);
+    Spirala s1(3,7,5);
+    cout<<s1;
  
 
     return 0;
