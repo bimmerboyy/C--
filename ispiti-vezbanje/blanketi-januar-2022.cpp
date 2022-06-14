9fe6f7bd433a569eee91d98295b5452188a087ca
bimmer <tarikkucevic@gmail.com>
2022-06-14 21:30:41 +0200

    ok

--- ispiti-vezbanje/blanketi-januar-2022.cpp
+++ ispiti-vezbanje/blanketi-januar-2022.cpp
@@ -1,5 +1,6 @@
 //Blanekti januar 2022
 
+#include <asm-generic/errno.h>
 #include<iostream>
 
 using namespace std;
@@ -13,6 +14,71 @@ class Figura{
     Figura(int x, int y){
         this->x = x;
         this->y = y;
+    }
+    virtual void dodaj(int x, int y){
+        this->x = x;
+        this->y = y;
+    }
+    virtual void ispisi(){
+        cout<<"("<<x<<","<<y<<")"<<endl;
+    }
+    
+};
+
+class Kvadrat : public Figura{
+    protected:
+    int velicinaStranice;
+    public:
+    Kvadrat(int x,int y,int velicinaStranice) : Figura(x,y) {
+        if(x || y || velicinaStranice < 0){
+            cout<<"Ponovo unesite stranice kvadrata"<<endl;
+        }
+        else{
+            this->velicinaStranice = velicinaStranice;
+        }
+    }
+
+   int PovrsinaKvadrata(){
+    return x*y*velicinaStranice;
+   }
+    int ObimKvadrata(){
+    return x+y+velicinaStranice;
+   }
+
+};
+
+class Pravougaonik : public Figura{
+    int a;
+    int b;
+    Pravougaonik(int a,int b, int x,int y,) : Figura(x,y) {
+        if (a || b || x || y < 0){
+            cout<<"Unsite ponovo stranice pravougaonika"<<endl;
+        }
+        else{
+            this->a = a;
+            this->b = b;
+        }
+    }
+    int PovrsinaPravougaonika(){
+       return  a*b*x*y;
+    }
+    int ObimPravougaonika(){
+       return  a+b+x+y;
+    }
+};
+
+class Kolekcija{
+    private:
+    Figura *figure;
+    public:
+    Kolekcija(Figura &figure){
+
+    }
+    int PovrsinaSvihFigura(){
+
+    }
+    int ObimSvihFigura(){
+        
     }
 };
 
