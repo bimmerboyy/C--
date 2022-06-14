714c77b310a906f13e53786a4585be30bebdc28f
anonymousvegan <majjunjulavgust@gmail.com>
2022-06-14 23:06:42 +0200

    blanketi 2022

--- ispiti-vezbanje/blanketi-januar-2022.cpp
+++ ispiti-vezbanje/blanketi-januar-2022.cpp
@@ -1,16 +1,15 @@
 //Blanekti januar 2022
-
 #include <asm-generic/errno.h>
 #include<iostream>
 
 using namespace std;
-
 
 class Figura{
     protected:
     int x;
     int y;
     public:
+    Figura(){}
     Figura(int x, int y){
         this->x = x;
         this->y = y;
@@ -22,36 +21,38 @@ class Figura{
     virtual void ispisi(){
         cout<<"("<<x<<","<<y<<")"<<endl;
     }
-    
+    virtual float povrsina() {
+        cout << "virtual \n";
+        return 0;
+    }
 };
 
 class Kvadrat : public Figura{
     protected:
     int velicinaStranice;
     public:
-    Kvadrat(int x,int y,int velicinaStranice) : Figura(x,y) {
-        if(x || y || velicinaStranice < 0){
+    Kvadrat(int velicinaStranice, int x = 0,int y = 0) : Figura(x,y) {
+        if(x < 0 || y < 0 || velicinaStranice < 0){
             cout<<"Ponovo unesite stranice kvadrata"<<endl;
         }
         else{
             this->velicinaStranice = velicinaStranice;
         }
     }
-
-   int PovrsinaKvadrata(){
-    return x*y*velicinaStranice;
-   }
+    float povrsina(){
+        return velicinaStranice*velicinaStranice;
+    }
     int ObimKvadrata(){
-    return x+y+velicinaStranice;
-   }
-
+        return 4 * velicinaStranice;
+    }
 };
 
 class Pravougaonik : public Figura{
     int a;
     int b;
-    Pravougaonik(int a,int b, int x,int y,) : Figura(x,y) {
-        if (a || b || x || y < 0){
+    public:
+    Pravougaonik(int a,int b, int x = 0,int y = 0) : Figura(x,y) {
+        if (a  < 0 || b < 0 || x < 0 || y < 0){
             cout<<"Unsite ponovo stranice pravougaonika"<<endl;
         }
         else{
@@ -59,30 +60,62 @@ class Pravougaonik : public Figura{
             this->b = b;
         }
     }
-    int PovrsinaPravougaonika(){
-       return  a*b*x*y;
+    float povrsina(){
+        return  a*b;
     }
     int ObimPravougaonika(){
-       return  a+b+x+y;
+        return 2*(a+b);
     }
 };
 
 class Kolekcija{
     private:
     Figura *figure;
+    int maksimalno_figura;
+    int trenutno_figura;
     public:
-    Kolekcija(Figura &figure){
-
+    Kolekcija(int maksimalno_figura){
+        this->maksimalno_figura = maksimalno_figura;
+        this->trenutno_figura = 0;
     }
-    int PovrsinaSvihFigura(){
-
+    void operator +=(Figura f){
+        if(trenutno_figura < maksimalno_figura){
+            Figura * temp = new Figura[trenutno_figura];
+            for(int i = 0; i < trenutno_figura; i++){
+                temp[i] = figure[i];
+            }
+            figure = new Figura[trenutno_figura+1];
+            for(int i = 0; i < trenutno_figura; i++){
+                figure[i] = temp[i];
+            }
+            figure[trenutno_figura] = f;
+            trenutno_figura++;
+        }
+        else{
+            maksimalno_figura += maksimalno_figura / 2;
+            *this += f;
+        }
     }
-    int ObimSvihFigura(){
-        
+    float povrsina_kolekcije(){
+        cout << "ušao";
+        float povrsina = 0;
+        for(int i = 0; i < trenutno_figura; i++){
+            povrsina += figure[i].povrsina();
+        }
+        return povrsina;
     }
 };
 
 int main(){
-
+    Kvadrat k1(4);
+    Pravougaonik p1(4,5);
+    Kvadrat k2(6);
+    Pravougaonik p2(6,7);
+    Kolekcija kolekcija(10);
+    kolekcija += k1;
+    kolekcija += p1;
+    kolekcija += k2;
+    kolekcija += p2;
+    cout << "Povrsina kolekcije: " << kolekcija.povrsina_kolekcije() << endl;
     return 0;
 }
