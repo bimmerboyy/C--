92ff84ada199c9868553a999448bb6ba8f8e466d
bimmer <tarikkucevic@gmail.com>
2022-06-14 20:39:46 +0200

    ok

--- ispiti-vezbanje/blanketi-januar-2022.cpp
+++ ispiti-vezbanje/blanketi-januar-2022.cpp
@@ -4,7 +4,19 @@
 
 using namespace std;
 
+
+class Figura{
+    protected:
+    int x;
+    int y;
+    public:
+    Figura(int x, int y){
+        this->x = x;
+        this->y = y;
+    }
+};
+
 int main(){
-    
+
     return 0;
 }
