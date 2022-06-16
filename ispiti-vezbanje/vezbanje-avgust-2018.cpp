93ca0ebf3852c430af2aa0db7ee2f7042ce98077
bimmer <tarikkucevic@gmail.com>
2022-06-16 17:48:50 +0200

    ok

---
+++ ispiti-vezbanje/vezbanje-avgust-2018.cpp
@@ -0,0 +1,66 @@
+// Realizovati sledeće klase :
+// (klase opremiti onim konstruktorima, destruktorima i operatorima dodele (preklopljenim operatorima)
+// koji su potrebni za bezbedno i efikasno korišćenje.
+// 1. Nalepnica ima naziv i jednoznačni automatski generisan identifikator kojem može da se pristupi. 
+//     Naziv se zadaje sa standardnog ulaza prilikom stvaranje objekta.
+//     Prilikom kopiranja voditi računa da se kopira naziv a da se identifikator sam kreira. 
+//     Omogućiti metodu za ispis. 
+// 2. Flaša pića ima sledeće podatke: 
+//     nalepnicu, cenu bez kaucije (u koju nije uračunata cena flaše), 
+//     zapreminu (podrazumevano 0,33 litara) i vrstu pića. 
+//     Vrsta pića može biti BEZALKOHOLNO (podrazumevano) i ALKOHOLNO.
+//     Svi podaci se unose prilikom stvaranja objekta i mogu se odštampati, a samo se cena može promeniti.
+//     Kreirati metodu koja može da izračuna cenu flaše pića sa kaucojom
+//     (ima razlike za staklenu i plastičnu flašu).
+//     Metodu PROVERA koja proverava da li su dve flaše jednake (flaše su jednake ukoliko imaju isti naziv nalepnice,
+//     cenu flaše sa kaucijom i vrstu pića. Omogućiti metodu za ispis.
+
+// 3. Staklena flaša pića je flaša pića kod koje se cena sa kaucijom računa tako što se na cenu bez kaucije doda 5% ukoliko
+//     je zapremina manja od 0,5 litara ili 10% ukoliko je zapremina veća ili jednaka od 0,5 litara.
+//     Preklopiti metodu ispis da ispisuje podatke o flaši i poruku da je flaša staklena.
+// 4. Plastična flaša pića je flaša pića kod koje je cena sa kaucijom jednaka ceni bez kaucije. Preklopiti metodu za ispis da 
+//     ispisuje podatke o flaši i poruku da je flaša plastnična.
+// 5. Formirati funkciju UKUPNO, van klasa koja formira dinamički niz staklenih i plastičnih flaša
+//     čije podatke unosi korisnik sa tastature i zatim računa i vraća ukupan zbir svih flaša sa kaucijom.
+// U main funkciji napraviti po jedan objekat svake klase i testirati sve metode, kao i funkciju UKUPNO.
+
+
+#include<iostream>
+
+
+//1. Nalepnica ima naziv i jednoznačni automatski generisan identifikator kojem može da se pristupi. 
+//     Naziv se zadaje sa standardnog ulaza prilikom stvaranje objekta.
+//     Prilikom kopiranja voditi računa da se kopira naziv a da se identifikator sam kreira. 
+//     Omogućiti metodu za ispis. 
+
+using namespace std;
+
+
+class Nalepnica{
+    protected:
+    string naziv;
+    int id;
+    static int brojac;
+    public:
+    Nalepnica(string naziv){
+        this->naziv = naziv;
+        id = brojac;
+        brojac++;
+    }
+    Nalepnica(Nalepnica &n1){
+        naziv = n1.naziv;
+        id = brojac;
+        brojac++;
+    }
+    void ispis(){
+        cout<<id<<"."<<naziv<<endl;
+    }
+    
+
+};
+
+int main(){
+    Nalepnica n1("Pepsi");
+    n1.ispis();
+    return 0;
+}
