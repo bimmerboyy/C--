a8d36eda2503e42fd70ec0acf09a50ac33225b0a
bimmer <tarikkucevic@gmail.com>
2022-06-16 12:01:23 +0200

    ok

--- ispiti-vezbanje/vezbanje-septembar-2020.cpp
+++ ispiti-vezbanje/vezbanje-septembar-2020.cpp
@@ -40,17 +40,12 @@
 
 using namespace std;
 
- //1.	Klasa Sastojak sadrži zadato ime i cenu po kilogramu.
-//      Pomoću metoda može da se dohvati vrsta sastojka 
-//      i da se izračuna cena za zadatu količinu sastojka u gramima(količina dolazi kao argument metode).
-//      Upisuje se u izlazni tok (cout<<s)  u obliku ime-cena/kg.
-//      Vrste slanog, slatkog i neutralnog sastojka su SLAN, SLADiNEUT, respektivno. (Odraditi pomoću enumeracije)
 
 
 enum Vrsta {SLAN,SLADAK,NEUTRALAN};
 
 class Sastojak{
-    protected:
+    private:
     Vrsta vrsta;
     string ime;
     int cenaPoKilogramu;
@@ -76,6 +71,33 @@ class Sastojak{
         return cenaPoKilogramu * kolicinaPoKilogarmu;
 
     }
+    friend ostream& operator<<(ostream& out,Sastojak &s1){
+        out<<s1.ime<<"-"<<s1.cenaPoKilogramu<<"/kg"<<endl;
+        return out;
+    }
+};
+
+ //Klasa Jelo ima ime i sadrži listu sastojaka, 
+//      kao i celobrojnu vrednost koja predstavlja količinu sastojka za pripremu jela izraženu u gramima.
+//      Cena jela je određena zbirom cene sastojaka,
+//      uz eventualno procentualno povećanje ili smanjenje osnovne cene,
+//      koje zavisi od sata formiranja cene jela. 
+//      Može da se postavi podatak o procentualnoj promeni cene jela i podatak o satu formiranja cene.
+//      Može da se doda sastojak određene količine u jelo, 
+//      pri čemu se pre dodavanja proverava da li je sastojak adekvatan(greška je ako nije),
+//      da se dohvati jednoslovna vrsta jela i da se izračuna njegova cena.
+//      Upisuje se u izlazni tok(cout<<j) tako što se u prvom redu upiše 
+//      ime:cena, a u narednim redovima se upisuje jedan sastojak poredu u obliku sastojak:količina. 
+//      Pri uništavanju jela, uništavaju se i svi sadržani sastojci.
+
+class Jelo{
+    private:
+    Sastojak *sastojci;
+    int kolicinaUGramima;
+    int sat;
+    int procenat;
+    public:
+    
 };
 
 
