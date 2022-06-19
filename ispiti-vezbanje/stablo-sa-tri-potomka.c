33f0e2e057fb2456ffe91f4d5643e3ad8d6bd1b2
bimmer <tarikkucevic@gmail.com>
2022-06-19 21:18:59 +0200

    ok

--- ispiti-vezbanje/stablo-sa-tri-potomka.c
+++ ispiti-vezbanje/stablo-sa-tri-potomka.c
@@ -3,6 +3,7 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 
 typedef struct tree{
@@ -15,13 +16,53 @@ typedef struct tree{
 TREE *root = NULL, *temp;
 
 
-struct tree *createNode(int info){
+struct tree *createTree(int info){
     temp = (struct tree *) malloc(sizeof(struct tree));
     temp->info = info;
     temp->left = NULL;
     temp->right = NULL;
     temp->middle = NULL;
     return temp;
+}
+
+void printInorder(struct tree *tree){
+    if(tree){
+        printInorder(tree->left);
+        printf("%d",tree->info);
+        printInorder(tree->middle);
+        printInorder(tree->right);
+    }
+}
+void printPreorder(struct tree *tree){
+    if(tree){
+        printf("%d",tree->info);
+        printInorder(tree->left);
+        printInorder(tree->middle);
+        printInorder(tree->right);
+    }
+}
+void printPostorder(struct tree *tree){
+    if(tree){
+        printInorder(tree->left);
+        printInorder(tree->middle);
+        printInorder(tree->right);
+        printf("%d",tree->info);
+    }
+}
+
+struct tree* addTree(struct tree *tree, int elem,char side){
+    if(root == NULL){
+        root = createTree(elem);
+        return root;
+    }
+    if(side == 'L'){
+        tree->left = addTree(tree->left,elem,'L');
+        return tree->left;
+    }
+    if(side == 'R'){
+        tree->right = addTree(tree->right,elem,'R');
+        return tree->right;
+    }
 }
 
 int main(){
