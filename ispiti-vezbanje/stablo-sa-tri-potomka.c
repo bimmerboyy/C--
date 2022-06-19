e4b53becf0dfe4f3caace4a68cee18f14fe4587f
bimmer <tarikkucevic@gmail.com>
2022-06-19 21:00:11 +0200

    ok

--- ispiti-vezbanje/stablo-sa-tri-potomka.c
+++ ispiti-vezbanje/stablo-sa-tri-potomka.c
@@ -2,9 +2,27 @@
 
 
 #include<stdio.h>
+#include<stdlib.h>
 
 
+typedef struct tree{
+    int info;
+    struct tree *left;
+    struct tree *right;
+    struct tree *middle;
+}TREE;
 
+TREE *root = NULL, *temp;
+
+
+struct tree *createNode(int info){
+    temp = (struct tree *) malloc(sizeof(struct tree));
+    temp->info = info;
+    temp->left = NULL;
+    temp->right = NULL;
+    temp->middle = NULL;
+    return temp;
+}
 
 int main(){
 
