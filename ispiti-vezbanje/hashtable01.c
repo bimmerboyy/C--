720f5eae9792ca7f97ee318279655559e1d48a6a
bimmer <tarikkucevic@gmail.com>
2022-06-20 20:23:51 +0200

    ok

--- ispiti-vezbanje/hashtable01.c
+++ ispiti-vezbanje/hashtable01.c
@@ -3,7 +3,26 @@
 #include<stdbool.h>
 #include<string.h>
 
+#define MAX_NAME 256
+#define TABLE_SIZE 10  
+
+//Kreiramo prvo strukturu gde ubacujemo podatke koje zelimo
+
+typedef struct person{
+    char name[MAX_NAME];
+    int age;
+
+}PERSON;
+
+//Pocinjemo od 5
+unsigned int hash(char *name) {
+    return 5;
+}
+
 
 int main(){
+    printf("Tarik => %u\n",hash("Tarik"));
+    printf("Nikola => %u\n",hash("Nikola"));
+    printf("Ermin => %u\n",hash("Ermin"));
     return 0;
 }
