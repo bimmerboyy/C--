6ec3654f3f39ea4c3bf9b92fe53e2cb9b46784ea
bimmer <tarikkucevic@gmail.com>
2022-06-20 21:19:10 +0200

    ok

--- ispiti-vezbanje/hashtable01.c
+++ ispiti-vezbanje/hashtable01.c
@@ -2,6 +2,8 @@
 #include<stdlib.h>
 #include<stdbool.h>
 #include<string.h>
+#include <sys/types.h>
+#include<stdint.h>
 
 #define MAX_NAME 256
 #define TABLE_SIZE 10  
@@ -16,7 +18,25 @@ typedef struct person{
 
 //Pocinjemo od 5
 unsigned int hash(char *name) {
-    return 5;
+    //Uzima prosecnu duzinu imena to je kod nas 5 jer Tarik i Ermin imaju 5 slova a Nikola 6
+    int lenght = strlen(name);
+    printf("Duzina niza imena je:%d\n", lenght);
+    //Kreira hash vrednost i stavlja je na 0
+    unsigned int hashValue = 0;
+    //For petljom prolazimo kroz niz imena koji je max 256 i on ce imati nasumicnu vrednost npr 86/256
+    //Tu vrednost ubacuje u hash vrednost 
+    for (int i = 0; i < lenght; i++){
+        hashValue += name[i];
+        printf("\nNiz imena je:%d\n",name[i]);
+        printf("\nHash vrednost je:%d\n",hashValue);
+        hashValue = (hashValue * name[i]) % TABLE_SIZE;
+        
+        printf("Kada se ubaci u tabelu hash vrednost je:%d\n",hashValue);
+    }
+    return hashValue;
+
+    
+    //return 5; Da ne bi ponavljao stalno 5 uzmemo duzinu niza  u koju ubacujemo imena
 }
 
 
