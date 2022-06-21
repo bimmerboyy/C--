7e851a818ab28555ba203691eb7996c1d64b23bb
bimmer <tarikkucevic@gmail.com>
2022-06-21 17:45:41 +0200

    ok

---
+++ ispiti-vezbanje/hashtable03.c
@@ -0,0 +1,5 @@
+/*Napisati program u jeziku C kojim se u hes tabelu duzine 20 upisuje 10 studenata pri cemu
+je podatak broj indeksa.Za upis koristiti podprogram.Za resavanje koalizije
+koristiti linearno hesiranje
+
+*/
