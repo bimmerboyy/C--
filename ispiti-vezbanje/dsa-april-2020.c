ed283ea43826a4d75671820e10f5c3b41b3a44e0
bimmer <tarikkucevic@gmail.com>
2022-06-28 18:17:59 +0200

    ok

---
+++ ispiti-vezbanje/dsa-april-2020.c
@@ -0,0 +1,10 @@
+/*Napisati program koji tezinski graf pamti kao matricu susedstva.Obezbediti funkciju za unos matrice susedstva
+Nakon unosa matrice program treba da ispise koje grane cini taj graf i granu najvece tezine 
+(npr. grana izmedju cvora 1 i 2  tezine 5) */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    return 0;
+}
