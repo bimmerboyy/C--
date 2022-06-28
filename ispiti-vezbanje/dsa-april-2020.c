4d0a50f4e81d00a5bc89ad47111843361de36c1a
bimmer <tarikkucevic@gmail.com>
2022-06-28 18:29:20 +0200

    ok

--- ispiti-vezbanje/dsa-april-2020.c
+++ ispiti-vezbanje/dsa-april-2020.c
@@ -5,6 +5,29 @@ Nakon unosa matrice program treba da ispise koje grane cini taj graf i granu naj
 #include<stdio.h>
 #include<stdlib.h>
 
+#define SIZE 20
+
+void unos(int matrix[SIZE][SIZE],int n){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            scanf("%d", &matrix[i][j]);
+        }
+        
+    }
+}
+void ispis(int matrix[SIZE][SIZE],int n){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            printf("%d ",matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 int main(){
+    int n,matrix[SIZE][SIZE];
+    printf("Unesite n:");
+    scanf("%d",&n);
+    
     return 0;
 }
