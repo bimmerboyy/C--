385338f94b0ed1c51aecad7be1279cb8440a9093
bimmer <tarikkucevic@gmail.com>
2022-07-01 16:07:15 +0200

    ok

--- ispiti-vezbanje/dsa-stabla-vezbanje-01.c
+++ ispiti-vezbanje/dsa-stabla-vezbanje-01.c
@@ -1,10 +1,19 @@
 //Napraviti funkciju za ispis stabla po nivoima od korena do zadnjeg nivoa od leva na desno
 
 #include<stdio.h>
+#include<stdlib.h>
 
 typedef struct tree{
-    
+    int data;
+    struct tree *left;
+    struct tree *right;
 }TREE;
+
+TREE *root = NULL, *temp;
+
+struct tree *createNode(int info){
+  temp = (struct tree *) malloc(sizeof(struct tree));
+}
 
 int main(){
 
